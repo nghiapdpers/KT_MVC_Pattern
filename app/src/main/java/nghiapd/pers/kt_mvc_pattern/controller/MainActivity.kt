2edb0cb25f81c784efeb2b06aeb4df2b33803c38
Pham Nghia <nghiapham.050503@gmail.com>
2023-04-22 09:21:18 +0700

    Initial Commit 2

--- app/src/main/java/nghiapd/pers/kt_mvc_pattern/controller/MainActivity.kt
+++ app/src/main/java/nghiapd/pers/kt_mvc_pattern/controller/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import nghiapd.pers.kt_mvc_pattern.APIHelper
 import nghiapd.pers.kt_mvc_pattern.R
 import nghiapd.pers.kt_mvc_pattern.adapter.UserAdapter
@@ -19,22 +20,30 @@ class MainActivity : AppCompatActivity() {
     lateinit var myRecyclerView: RecyclerView;
     lateinit var myAdapter: UserAdapter;
     private var data = ArrayList<User>();
+    lateinit var myRefreshLayout: SwipeRefreshLayout;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         myRecyclerView = findViewById(R.id.myRecyclerView);
+        myRefreshLayout = findViewById(R.id.swipeToRefresh);
 
         myAdapter = UserAdapter(data);
         myRecyclerView.adapter = myAdapter;
         myRecyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false);
 
-        loadData()
+        loadData();
+
+        myRefreshLayout.setOnRefreshListener {
+            data.clear();
+            loadData();
+            myRefreshLayout.isRefreshing = false;
+        }
     }
 
     private fun loadData(){
-        var result = APIHelper.apiService.getUser(10)
+        var result = APIHelper.apiService.getUser(10);
         result.enqueue(object : Callback<UserRespone> {
             override fun onResponse(call: Call<UserRespone>, response: Response<UserRespone>) {
                 data.addAll(response.body()!!.results)
